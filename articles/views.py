39b9abcacce96d7af47835a14961b2a7c5d24a83
Letiana0622 <letia@mail.ru>
2023-01-21 13:24:38 +0300

    task2 corrections done

--- articles/views.py
+++ articles/views.py
@@ -8,10 +8,10 @@ def articles_list(request):
     template = 'articles/news.html'
     article_object = Article.objects.all()
     articles = article_object.order_by('-published_at')
-    scopes = Article.scopes.all()
+    # scopes = Article.scopes.all()
     # scopes = scope_object.order_by('-topic')
     # context = {'articles': articles}
-    context = {'articles': articles, 'scopes': scopes}
+    context = {'articles': articles}
 
     # используйте этот параметр для упорядочивания результатов
     # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
